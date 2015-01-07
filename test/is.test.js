fa57de0f9c3d6d809af8e6b8573d8c396d4b5179
Clint Walker <cw@neonrelic.com>
2015-01-07 15:43:08 +0800

    Add tests for .is

--- test/is.test.js
+++ test/is.test.js
@@ -4,26 +4,86 @@ var is = require('../is');
 
 describe('.is', function () {
 
-  it('array', function () {
-    expect( is.array([1,2]) ).to.be.true;
-    expect( is.array( new Array() ) ).to.be.true;
-    expect( is.array( {0:1, 1:2, 2:3} ) ).to.be.false;
+  it('string', function () {
+    expect( is.string('') ).to.eql(true);
+    expect( is.string('true') ).to.eql(true);
+    expect( is.string(true) ).to.eql(false);
+    expect( is.string(new Date()) ).to.eql(false);
   });
 
-  it('boolean');
+  it('integer', function () {
+    expect( is.integer(0) ).to.eql(true);
+    expect( is.integer(-15) ).to.eql(true);
+    expect( is.integer(100) ).to.eql(true);
+    expect( is.integer(1.0) ).to.eql(true);
+    expect( is.integer(1.2) ).to.eql(false);
+    expect( is.integer(NaN) ).to.eql(false);
+  });
 
-  it('date');
+  it('number', function () {
+    expect( is.number(0) ).to.eql(true);
+    expect( is.number(-15) ).to.eql(true);
+    expect( is.number(100) ).to.eql(true);
+    expect( is.number(1.0) ).to.eql(true);
+    expect( is.number(1.2) ).to.eql(true);
+    expect( is.number(NaN) ).to.eql(false);
+  });
 
-  it('equal');
+  it('array', function () {
+    expect( is.array(0) ).to.eql(false);
+    expect( is.array('1,2,3') ).to.eql(false);
+    /*jshint -W009 */
+    expect( is.array(new Array()) ).to.eql(true);
+    expect( is.array([]) ).to.eql(true);
+  });
 
-  it('function');
+  it('boolean', function () {
+    expect( is.boolean(0) ).to.eql(false);
+    expect( is.boolean(1) ).to.eql(false);
+    expect( is.boolean(-1) ).to.eql(false);
+    expect( is.boolean(true) ).to.eql(true);
+    expect( is.boolean(false) ).to.eql(true);
+    expect( is.boolean('true') ).to.eql(false);
+  });
 
-  it('integer');
+  it('object', function () {
+    expect( is.object(function(){}) ).to.eql(false);
+    expect( is.object('object') ).to.eql(false);
+    expect( is.object(new Date()) ).to.eql(false);
+    expect( is.object([]) ).to.eql(false);
+    expect( is.object({}) ).to.eql(true);
+  });
 
-  it('number');
+  it('date', function () {
+    expect( is.date(0) ).to.eql(false);
+    expect( is.date('28 July 1914') ).to.eql(false);
+    expect( is.date(new Date()) ).to.eql(true);
+  });
 
-  it('string');
+  it('function', function () {
+    expect( is.function({}) ).to.eql(false);
+    expect( is.function(function () {}) ).to.eql(true);
+    expect( is.function('function() {}') ).to.eql(false);
+    /* jshint evil: true */
+    expect( is.function(new Function())).to.eql(true);
+  });
 
-  it('undefined');
+  it('undefined', function () {
+    expect( is.undefined(0) ).to.eql(false);
+    expect( is.undefined(null) ).to.eql(false);
+    expect( is.undefined('undefined') ).to.eql(false);
+    expect( is.undefined() ).to.eql(true);
+    expect( is.undefined(undefined) ).to.eql(true);
+  });
+
+  it('equal', function () {
+    expect( is.equal(1,1) ).to.eql(true);
+    expect( is.equal(false, false) ).to.eql(true);
+    expect( is.equal([1,2,3], [1,2,3]) ).to.eql(true);
+    expect( is.equal({a:{b:1}}, {a:{b:1}}) ).to.eql(true);
+    expect( is.equal([{a:1}],[{a:1}]) ).to.eql(true);
+    expect( is.equal([{a:1}],[{a:2}]) ).to.eql(false);
+    expect( is.equal({a:{b:1}},{a:{b:2}}) ).to.eql(false);
+  });
 
 });
